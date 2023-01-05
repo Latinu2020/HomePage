19e8b9b111c554e22998be75f77a290113d22495
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 18:44:59 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -4,13 +4,11 @@ st.header(":mailbox: Get In Touch With Me!")
 
 
 contact_form = """
-<form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
-     <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email" required>
-     <textarea name="message" placeholder="Your message here"></textarea>
-     <button type="submit">Send</button>
-</form>
+<form action="https://formsubmit.co/your@email.com" method="POST">
+     <input type="text" name="name" required>
+     <input type="email" name="email" required>
+     <button type="submit">Send</button>
+</form> 
 """
 
 st.markdown(contact_form, unsafe_allow_html=True)
