453a2da5bd3f5862b54f112aeccae72543d4863d
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 21:44:11 +0200

    update

--- pages/5_🎓_MySkill.py
+++ pages/5_🎓_MySkill.py
@@ -8,7 +8,7 @@ from PIL import Image
 current_dir= Path(__file__).parent if "__file__"in locals() else Path.cwd()
 udemy_pic= current_dir / "skill" / "udemy.jpg"
 udemy_pic_read=Image.open(udemy_pic)
-autodesk_pic= current_dir / "skill" / "autodesk.png"
+autodesk_pic= current_dir / "skill" / "autodesk.jpg"
 autodesk_pic_read=Image.open(autodesk_pic)
 st.set_page_config(page_title="Skill", page_icon="📊")
 
@@ -18,8 +18,8 @@ st.markdown("####.")
 col1,col2=st.columns(2)
 with col1:
     st.subheader("UDEMY")
-    st.image(udemy_pic_read, width=300)
+    st.image(udemy_pic_read, width=200)
 with col2:
-    st.subheader("AUTODESK")
-    st.image(autodesk_pic_read, width=300)
+    st.subheader("AUTODESK UNIVERSITY")
+    st.image(autodesk_pic_read, width=200)
    
