672cbe55056aeb023783c67db7c9a84a1597a74f
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 18:05:35 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -1,6 +1,4 @@
 import streamlit as st
-from utils.helper import send_email
-from utils.constants import (SMTP_SERVER_ADDRESS, PORT, SENDER_PASSWORD,SENDER_ADDRESS)
 
 if __name__=='__main__':
 
@@ -12,18 +10,3 @@ if __name__=='__main__':
             uploaded_file=st.file_uploader("Attachment")
             submit_res= st.form_submit_button(label='Send')
 
-            if submit_res:
-
-                extra_info="""
-
-                -------------------------------------------
-                Email Address of Sender {}  \n
-                Sender Full Name {} \n
-                ------------------------------------------- \n  \n
-                            """.format(email,fullName)
-
-                message=extra_info + text
-
-                send_email(sender=SENDER_ADDRESS, password=SENDER_PASSWORD,
-                 receiver=email, smtp_server=SMTP_SERVER_ADDRESS,smtp_port=PORT,
-                 email_message=message, subject=subject, attachment=uploaded_file)
