0fd2dd915b22e35e5ebd8cf644a6ccd937045017
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 19:11:31 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 form = st.form("my_form")
-form.slider("Inside the form")
-st.slider("Outside the form")
+form.text_input('Your email address')
+form.text_input('Sent email address')
+form.text_input('Subject')
+form.text_area('Description Email')
 
 # Now add a submit button to the form:
 form.form_submit_button("Submit")
-
