20aa7b18654c69572941fc8669be42ab5628adad
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 18:10:39 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -1,34 +1,13 @@
 import streamlit as st
 
-formbtn = st.button("Form")
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
 
-if "formbtn_state" not in st.session_state:
-    st.session_state.formbtn_state = False
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
 
-if formbtn or st.session_state.formbtn_state:
-    st.session_state.formbtn_state = True
-    
-    st.subheader("User Info Form")
-    # name = st.text_input("Name")
-    with st.form(key = 'user_info'):
-        st.write('User Information')
-    
-        name = st.text_input(label="Name 📛")
-        age = st.number_input(label="Age 🔢", value=0)
-        email = st.text_input(label="Email 📧")
-        phone = st.text_input(label="Phone 📱")
-        gender = st.radio("Gender 🧑", ("Male", "Female", "Prefer Not To Say"))
-    
-        submit_form = st.form_submit_button(label="Register", help="Click to register!")
-    
-        # Checking if all the fields are non empty
-        if submit_form:
-            st.write(submit_form)
-    
-            if name and age and email and phone and gender:
-                # add_user_info(id, name, age, email, phone, gender)
-                st.success(
-                            f"ID:  \n Name: {name}  \n Age: {age}  \n Email: {email}  \n Phone: {phone}  \n Gender: {gender}"
-                        )
-            else:
-                st.warning("Please fill all the fields")
+st.write("Outside the form")
