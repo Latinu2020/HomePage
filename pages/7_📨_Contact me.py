231e08e28159d459a9d5c181f007ddb5b7ac4f15
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 19:01:34 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-st.number_input('Pick a number', 0,10)
-st.text_input('Email address')
-st.date_input('Travelling date')
-st.time_input('School time')
-st.text_area('Description')
-st.file_uploader('Upload a photo')
-st.color_picker('Choose your favorite color')
+st.text_input('Your email address')
+st.text_input('Sent email address')
+st.text_input('Subject')
+st.text_area('Description Email')
+
+
