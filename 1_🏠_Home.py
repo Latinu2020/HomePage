10cf8b7e039a3f16589220685a63f40cc77b278c
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 11:32:39 +0200

    update

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -9,6 +9,7 @@ st.write("# Welcome to My Page 💙")
 
 st.sidebar.success("Home")
 st.info('This is a purely informational message', icon="ℹ️")
+st.success('This is a success message!', icon="✅")
 
 st.markdown(
     """
