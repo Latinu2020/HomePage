673a7d8be59aec64691d7a3494fc2b21acc684a3
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 19:44:54 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+with open('style/style.css') as f:
+    st.markdown(f'<style>{f.read()}</style', unsafe_allow_html=True)
+
 form = st.form("my_form")
 form.text_input('Your email address')
 form.text_input('Sent email address')
