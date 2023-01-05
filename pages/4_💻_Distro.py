7ae90a142436d0411d6d280c47f13f6167d96df6
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 21:16:57 +0200

    upgrade

--- pages/4_💻_Distro.py
+++ pages/4_💻_Distro.py
@@ -23,12 +23,12 @@ st.markdown("A set of software components, often open source, that have been pac
 
 col1,col2,col3,col4,col5=st.columns(5)
 with col1:
-    st.image(linux_pic_read, width=230)
+    st.image(linux_pic_read, width=150)
 with col2:
-    st.image(unix1_pic_read, width=230)
+    st.image(unix1_pic_read, width=150)
 with col3:
-    st.image(unix2_pic_read, width=230)
+    st.image(unix2_pic_read, width=150)
 with col4:
-    st.image(unix3_pic_read, width=230)
+    st.image(unix3_pic_read, width=150)
 with col5:
-    st.image(windows_pic_read, width=230)    
+    st.image(windows_pic_read, width=150)    
