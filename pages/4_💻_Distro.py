8b73f7360d938468cef8e5a2b3c896aa391dea69
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 20:50:16 +0200

    update

--- pages/4_💻_Distro.py
+++ pages/4_💻_Distro.py
@@ -5,4 +5,4 @@ st.set_page_config(page_title="Distro", page_icon="📊")
 st.title("# Distro")
 st.markdown("A set of software components, often open source, that have been packaged into a larger product or component for distribution to end-users.")
 
-st.image("distro\freebsd.png",width=230)
+st.image("distro\freebsd.png")
