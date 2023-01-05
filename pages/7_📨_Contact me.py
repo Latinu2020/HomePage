bcb2598cef8a060b9350962315113ea205d0c539
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 18:02:47 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -1,56 +1,29 @@
 import streamlit as st
-import pandas as pd
-import altair as alt
-from urllib.error import URLError
+from utils.helper import send_email
+from utils.constants import (SMTP_SERVER_ADDRESS, PORT, SENDER_PASSWORD,SENDER_ADDRESS)
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
+if __name__=='__main__':
 
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
-st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-)
+        with st.form("Email Form"):
+            subject =st.text_input(label='Subject',placeholder="Please enter subject of your mail")
+            fullName =st.text_input(labe='Full Name', placeholder="PLease enter your full name")
+            email=st.text_input(label='Email Address', placeholder= "Please enter your email address")
+            text =st.text_area(label='Email Text', placeholder="PLease enter your text here")
+            uploaded_file=st.file_uploader("Attachment")
+            submit_res= st.form_submit_button(label='Send')
 
+            if submit_res:
 
-@st.cache
-def get_UN_data():
-    AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-    return df.set_index("Region")
+                extra_info="""
 
+                -------------------------------------------
+                Email Address of Sender {}  \n
+                Sender Full Name {} \n
+                ------------------------------------------- \n  \n
+                            """.format(email,fullName)
 
-try:
-    df = get_UN_data()
-    countries = st.multiselect(
-        "Choose countries", list(df.index), ["China", "United States of America"]
-    )
-    if not countries:
-        st.error("Please select at least one country.")
-    else:
-        data = df.loc[countries]
-        data /= 1000000.0
-        st.write("### Gross Agricultural Production ($B)", data.sort_index())
+                message=extra_info + text
 
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=["index"]).rename(
-            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        )
-        chart = (
-            alt.Chart(data)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="year:T",
-                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                color="Region:N",
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e.reason
-    )
+                send_email(sender=SENDER_ADDRESS, password=SENDER_PASSWORD,
+                 receiver=email, smtp_server=SMTP_SERVER_ADDRESS,smtp_port=PORT,
+                 email_message=message, subject=subject, attachment=uploaded_file)
