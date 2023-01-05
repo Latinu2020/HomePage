dbd65058f05e4a662d23cebead0c734e2b9a2763
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 20:21:04 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('Contact me')
 #with st.form("my_form"):
 form = st.form("my_form")
-form.text_input(':yellow[Your email address]')
+form.text_input(":yellow[Your email address]")
 form.text_input('Sent email address')
 form.text_input('Subject')
 form.text_area('Description Email')
