0459cb0085a2292f2f1c71d00adddd493ebe1e14
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 20:06:44 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -1,15 +1,8 @@
-from pathlib import Path
 import streamlit as st
-
-
-current_dir= Path(__file__).parent if "__file__"in locals() else Path.cwd()
-css_file=current_dir / "styles" / "style.css"
-with open(css_file) as f:
     form = st.form("my_form")
-    form.text_input('Your email address',"<style>{}</style".format(f.read()), unsafe_allow_html=True)
-    form.text_input('Sent email address',"<style>{}</style".format(f.read()), unsafe_allow_html=True)
-    form.text_input('Subject',"<style>{}</style".format(f.read()), unsafe_allow_html=True)
-    form.text_area('Description Email',"<style>{}</style".format(f.read()), unsafe_allow_html=True)
-
+    form.text_input('Your email address')
+    form.text_input('Sent email address')
+    form.text_input('Subject')
+    form.text_area('Description Email')
 # Now add a submit button to the form:
     form.form_submit_button("Submit")
