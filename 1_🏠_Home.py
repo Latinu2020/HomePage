cc5ce1b7e2b362e5c68292ef9d7303180d13b2bc
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 16:46:08 +0200

    UPDATE

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -28,3 +28,27 @@ PROJECTS={
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 st.title("Hello there!")
+
+#--LOAD CSS, PDF & PROFIL PIC
+
+with open(css_file) as f:
+    st.markdown("<style>{}</style".format(f.read()), unsafe_allow_html=True)
+with open(resume_file, "rb") as pdf_file:
+    PDFbyte=pdf_file.read()
+profile_pic=Image.open(profile_pic)
+
+######--hero section
+
+col1,col2=st.columns(2,gap="small")
+with col1:
+    st.image(profile_pic, width=230)
+with col2:
+    st.title(NAME)
+    st.write(DESCRIPTION) 
+    st.download_button(
+        label="  Download Resume",
+        data=PDFbyte,
+        file_name=resume_file.name,
+        mime="application/octet-stream"
+    )
+    st.write("✉",EMAIL)   
