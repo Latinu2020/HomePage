88571c2a4a64d79d03782ec624d5cd3e15190edf
Latinu2020 <florin.rot33@gmail.com>
2023-01-05 18:51:21 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -1,21 +1,29 @@
-import streamlit as st  # pip install streamlit
+import streamlit as st
 
-st.header(":mailbox: Get In Touch With Me!")
+form_container = st.sidebar.container()
+def get_customers():
+    return [f'Customer {i+1}' for i in range(10)]
+
+def submitted():
+    st.session_state.submitted = True
+def reset():
+    st.session_state.submitted = False
 
 
-contact_form = """
-<form action="https://formsubmit.co/your@email.com" method="POST">
-     <input type="text" name="name" required>
-     <input type="email" name="email" required>
-     <button type="submit">Send</button>
-</form> 
-"""
-
-st.markdown(contact_form, unsafe_allow_html=True)
-
-# Use Local CSS File
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-local_css("style/style.css")
+if st.sidebar.button("Add User", on_click=reset):
+    with form_container:
+        with st.form("add user"):
+            st.write("Add User")
+            email = st.text_input("Email", key='new_email')
+            customers = get_customers()
+            customer = st.selectbox("Customer",customers, key='new_customer')
+            password = st.text_input("Password", key='new_password')
+            st.form_submit_button(label="Submit", on_click=submitted)
+if 'submitted' in st.session_state:
+    if st.session_state.submitted == True:
+        print("Submit button pressed")
+        new_user = {'email':st.session_state.new_email,
+                     'customer':st.session_state.new_customer,
+                     'password':st.session_state.new_password}
+        st.sidebar.write(new_user)
+        reset() # Prevents rerunning new user creation on next page load
