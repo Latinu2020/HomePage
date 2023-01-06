cbcd6fdf07f4f57a15d9090bea76dc9060ec8662
Latinu2020 <florin.rot33@gmail.com>
2023-01-06 12:24:16 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -15,3 +15,9 @@ contact_form="""
 """
 
 st.markdown(contact_form, unsafe_allow_html=True)
+
+#use local css File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style", unsafe_allow_html=True)
+local_css("style/style.css")        
