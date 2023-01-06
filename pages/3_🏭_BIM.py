7b2f3ddf613a74ad268c27ae3bdf40ae1b48935c
Latinu2020 <florin.rot33@gmail.com>
2023-01-06 19:19:57 +0200

    UPDATE

--- pages/3_🏭_BIM.py
+++ pages/3_🏭_BIM.py
@@ -4,8 +4,13 @@ st.set_page_config(page_title="BIM", page_icon="🌍")
 
 st.markdown("## BIM")
 st.write(
-    """This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
+    """Building information modeling (BIM) is a process supported by various tools, technologies and contracts
+    involving the generation and management of digital representations of physical and functional characteristics of places. 
+    Building information models (BIMs) are computer files (often but not always in proprietary formats and containing
+    proprietary data) which can be extracted, exchanged or networked to support decision-making regarding a built asset. 
+    BIM software is used by individuals, businesses and government agencies who plan, design, construct, operate and maintain
+    buildings and diverse physical infrastructures, such as water, refuse, electricity, gas, communication utilities, roads, 
+    railways, bridges, ports and tunnels.
+    """
 )
 
