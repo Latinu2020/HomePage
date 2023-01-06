4da827af00067bc48d5d64526b69f07fc26188c2
Latinu2020 <florin.rot33@gmail.com>
2023-01-06 13:38:06 +0200

    update

--- pages/6_➥_Code.py
+++ pages/6_➥_Code.py
@@ -2,11 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="CODE", page_icon="📊")
 
-st.markdown("# CODE")
-st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-)
-
-"""### gif from url"""
-st.markdown("![Alt Text](https://media.giphy.com/media/7FrOU9tPbgAZtxV5mb/giphy-downsized-large.gif)")
+"""### Give me a Code"""
+st.markdown("![Alt Text](https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif)")
