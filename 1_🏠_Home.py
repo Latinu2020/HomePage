be8582e83a4e0934e47fa49ddbaf9ec1983daff7
Latinu2020 <florin.rot33@gmail.com>
2023-01-06 13:58:21 +0200

    update

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -27,8 +27,7 @@ PROJECTS={
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 """### Welcome!"""
-st.markdown("![Alt Text](https://media.giphy.com/media/LR5ZBwZHv02lmpVoEU/giphy-downsized-large.gif")
-
+st.markdown("![Alt Text](https://media.giphy.com/media/LR5ZBwZHv02lmpVoEU/giphy-downsized-large.gif)")
 #st.title("Hello there!")
 
 #--LOAD CSS, PDF & PROFIL PIC
