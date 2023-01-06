b82c7060585f6a98c9ce62a197ec78d2653f1636
Latinu2020 <florin.rot33@gmail.com>
2023-01-06 12:38:36 +0200

    update

--- pages/7_📨_Contact me.py
+++ pages/7_📨_Contact me.py
@@ -7,10 +7,10 @@ st.title('Contact me')
 contact_form="""
 <form action="https://formsubmit.co/florin_rot@yahoo.com" method="POST">
     <input type="hidden" name="_captcha" value="false">
-    <input type="text" name="name" placeholder="Your name" required>
-    <input type="email" name="email" placeholder="Your email" required>
+    <input type="text" name="name" placeholder="Your name" required>
+    <input type="email" name="email" placeholder="Your email" required>
     <textarea name="message" placeholder="Your message here"></textarea>
-    <button type="submit">Send</button>
+    <button type="submit">Send</button>
 </form>
 """
 
