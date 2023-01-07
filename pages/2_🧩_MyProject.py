af6da6421c66af21bec48b2b1084700a772a8020
Latinu2020 <florin.rot33@gmail.com>
2023-01-07 10:10:47 +0200

    update

--- pages/2_🧩_MyProject.py
+++ pages/2_🧩_MyProject.py
@@ -2,5 +2,13 @@ import streamlit as st
 
 st.set_page_config(page_title="My Project", page_icon="📈")
 
+
+html_page="""
+<h1 style="border:2px solid Tomato;">Hello World</h1>
+"""
+
+
+st.markdown(html_page.format(),unsafe_allow_html=True)
+
 """### My Project"""
 st.markdown("![Alt Text](https://media.giphy.com/media/fixyBa6ainp5yUQPhU/giphy.gif)")
