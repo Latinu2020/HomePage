486b810c2febe66c416677cb492e2b8ecc18e3ee
Latinu2020 <florin.rot33@gmail.com>
2023-01-07 10:44:02 +0200

    update

--- pages/2_🧩_MyProject.py
+++ pages/2_🧩_MyProject.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="My Project", page_icon="📈",layout="wide")
 
 html_page="""
 <div style="width: 600px; padding: 50px;"</div>
-<h1 style="border:3px solid Tomato; border-radius: 12px; text-align: center; text-shadow: 2px 2px 5px red; font-family: Arial;">My Project</h1>
+<h1 style="border:3px solid Tomato; border-radius: 12px; text-align: center; text-shadow: 2px 2px 5px red; font-family: Arial;">  My Project</h1>
 """
 
 
