53396ab080a1d089ce6617538e0f9fb4c9531460
Latinu2020 <florin.rot33@gmail.com>
2023-01-07 11:11:23 +0200

    update

--- pages/2_🧩_MyProject.py
+++ pages/2_🧩_MyProject.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit_extras.switch_page_button import switch_page
 
 st.set_page_config(page_title="My Project", page_icon="📈",layout="wide")
 
