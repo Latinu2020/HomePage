94710f95f34ebfbd02719758e0538b5edc4e79f0
Latinu2020 <florin.rot33@gmail.com>
2023-01-07 11:15:21 +0200

    update

--- pages/2_🧩_MyProject.py
+++ pages/2_🧩_MyProject.py
@@ -1,29 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
-from streamlit_extras.switch_page_button import switch_page
-
-st.set_page_config(page_title="My Project", page_icon="📈",layout="wide")
-
-
-def navigation_bar():
-    with st.container():
-        selected = option_menu(
-            menu_title=None,
-            options=["Home", "Upload", "Analytics", 'Settings', 'Contact'],
-            icons=['house', 'cloud-upload', "graph-up-arrow", 'gear', 'phone'],
-            menu_icon="cast",
-            orientation="horizontal",
-            styles={
-                "nav-link": {
-                    "text-align": "left",
-                    "--hover-color": "#eee",
-                }
-            }
-        )
-        if selected == "Analytics":
-            switch_page("Analytics")
-        if selected == "Contact":
-            switch_page("Contact")
 
 html_page="""
 <div style="width: 600px; padding: 50px;"</div>
