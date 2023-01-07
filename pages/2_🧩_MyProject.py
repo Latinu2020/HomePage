783bcc58a969f3b9fa5f3941e5fc59a76182bb90
Latinu2020 <florin.rot33@gmail.com>
2023-01-07 10:58:18 +0200

    update

--- pages/2_🧩_MyProject.py
+++ pages/2_🧩_MyProject.py
@@ -4,8 +4,7 @@ st.set_page_config(page_title="My Project", page_icon="📈",layout="wide")
 
 
 html_page="""
-@keyframes example {from {background-color: red;}to {background-color: yellow;}
-<div style="width: 600px; padding: 50px; animation-name: example; animation-duration: 7s;"</div>
+<div style="width: 600px; padding: 50px; animation-name: example; animation: 3s infinite alternate slidein;"</div>
 <h1 style="border:3px solid Tomato; border-radius: 12px; text-align: center; text-shadow: 2px 2px 5px red; font-family: Arial;">  My Project</h1>
 """
 
