ae3ffb3a201734f04afbf4b9634d73f00cfe7f4e
Latinu2020 <florin.rot33@gmail.com>
2023-01-07 11:08:53 +0200

    update

--- pages/2_🧩_MyProject.py
+++ pages/2_🧩_MyProject.py
@@ -4,12 +4,7 @@ st.set_page_config(page_title="My Project", page_icon="📈",layout="wide")
 
 
 html_page="""
-<.header style="
-  background-color: #F1F1F1;
-  text-align: center;
-  padding: 20px;"
-  </.header>
-
+.nou { grid-area: nou; }
 <div style="width: 600px; padding: 50px;"</div>
 <h1 style="border:3px solid Tomato; border-radius: 16px; text-align: center; text-shadow: 2px 2px 5px red; font-family: Arial;">  My Project</h1>
 """
