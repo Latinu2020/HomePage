f569876b33a36bb3b62b2de387e2c0c341aa5171
Latinu2020 <florin.rot33@gmail.com>
2023-01-07 10:21:14 +0200

    update

--- pages/2_🧩_MyProject.py
+++ pages/2_🧩_MyProject.py
@@ -4,41 +4,23 @@ st.set_page_config(page_title="My Project", page_icon="📈")
 
 
 html_page="""
-    <!DOCTYPE html>  
-    <html>  
-    <head>  
-    <style>   
-    div {  
-        width: 100px;  
-        height: 100px;  
-        background: red;  
-        position: relative;  
-        -webkit-animation: myfirst 5s; /* Chrome, Safari, Opera */  
-        animation: myfirst 5s;  
-    }  
-    /* Chrome, Safari, Opera */  
-    @-webkit-keyframes myfirst {  
-        0%   {background:red; left:0px; top:0px;}  
-        25%  {background:yellow; left:300px; top:0px;}  
-        50%  {background:blue; left:200px; top:300px;}  
-        75%  {background:green; left:0px; top:200px;}  
-        100% {background:red; left:0px; top:0px;}  
-    }  
-    /* Standard syntax */  
-    @keyframes myfirst {  
-        0%   {background:red; left:0px; top:0px;}  
-        25%  {background:yellow; left:300px; top:0px;}  
-        50%  {background:blue; left:300px; top:200px;}  
-        75%  {background:green; left:0px; top:200px;}  
-        100% {background:red; left:0px; top:0px;}  
-    }  
-    </style>  
-    </head>  
-    <body>  
-    <p><b>Note:</b> The Internet Explorer 9 and its earlier versions don't support this example.</p>  
-    <div></div>  
-    </body>  
-    </html>  
+<head>
+<meta charset="utf-8">
+<title>Effect of CSS Padding on Element Box Size</title>
+<style>
+    div {
+        width: 100%;
+        padding: 25px;
+        background: violet;
+    }
+</style>
+</head>
+<body>
+    <div>
+        <h1>This is a DIV Box</h1>
+    </div>
+    <p><strong>Notice</strong>, the scrollbar at the bottom of the viewport.</p>
+</body>
 """
 
 
