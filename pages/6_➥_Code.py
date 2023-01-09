0b0889b5b5dddcdc4fb16571c9d62090343a45d7
Latinu2020 <64733294+Latinu2020@users.noreply.github.com>
2023-01-09 21:15:02 +0200

    update

--- pages/6_➥_Code.py
+++ pages/6_➥_Code.py
@@ -11,8 +11,14 @@ logo_pic_read=Image.open(logo_pic)
 
 st.set_page_config(page_title="CODE", page_icon="📊")
 
-"""### Give me a Code"""
+html_page="""
+<div style="width: 600px; padding: 50px;"</div>
+<h1 style="border:3px solid Tomato; border-radius: 16px; text-align: center; text-shadow: 2px 2px 5px red; font-family: Arial;">  Code</h1>
+"""
+st.markdown(html_page.format(),unsafe_allow_html=True)
+
+st.image(logo_pic_read)
+
 st.markdown("![Alt Text](https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif)")
 
 
-st.image(logo_pic_read)
