fc4e1ec5f37631e3871f58de1cc3f0bcc9dd8465
Latinu2020 <64733294+Latinu2020@users.noreply.github.com>
2023-01-09 21:11:30 +0200

    update

--- pages/6_➥_Code.py
+++ pages/6_➥_Code.py
@@ -1,6 +1,18 @@
+from pathlib import Path
 import streamlit as st
+from PIL import Image
+
+
+
+current_dir= Path(__file__).parent if "__file__"in locals() else Path.cwd()
+logo_pic= current_dir / "Code" / "Logo.png"
+logo_pic_read=Image.open(logo_pic)
+
 
 st.set_page_config(page_title="CODE", page_icon="📊")
 
 """### Give me a Code"""
 st.markdown("![Alt Text](https://media.giphy.com/media/xT9IgzoKnwFNmISR8I/giphy.gif)")
+
+
+st.image(logo_pic_read, width=100)
