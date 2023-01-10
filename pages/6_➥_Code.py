1b400de0738dc41bb8a2a7761b9fe181394e65e5
Latinu2020 <64733294+Latinu2020@users.noreply.github.com>
2023-01-10 20:25:59 +0200

    update

--- pages/6_➥_Code.py
+++ pages/6_➥_Code.py
@@ -12,7 +12,7 @@ logo_pic_read=Image.open(logo_pic)
 st.set_page_config(page_title="CODE", page_icon="📊",layout="wide")
 html_page="""
 <div style="width: 600px; padding: 50px;"</div>
-<h1 style="border:3px solid Tomato; border-radius: 16px; text-align: center; text-shadow: 2px 2px 5px red; font-family: Arial;">  Code</h1>
+<h1 style="text-align: center; text-shadow: 2px 2px 5px red; font-family: Arial;">  Code</h1>
 """
 
 col1, col2, col3 = st.columns(3)
