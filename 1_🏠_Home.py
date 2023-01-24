b2f054ca4fc4ebf1f88ec7d4e5e568b7aad7421d
Latinu2020 <florin_rot@yahoo.com>
2023-01-24 15:43:18 +0200

    UPDATE

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -31,7 +31,7 @@ EXPERIENCE="""
 
 """
 SKILL="""
-<div style="width: 1050px; padding: 0px;"</div>
+<div style="width: 1200px; padding: 0px;"</div>
 <h1 style="text-align: left; text-indent: 70px; color: white; font-size: 50px; text-shadow: 5px 5px 10px #000000; font-family: "Fantasy">
 
 🏅Team player/leader capable of manage multidiscipline modellers and drafters. Recently working as project BIM manager on multidiscipline projects and managing modellers from around Australia and overseas
