890166fb41ee99117a192db56479a64eeaa4479f
Latinu2020 <florin_rot@yahoo.com>
2023-01-24 15:49:04 +0200

    update

--- 1_🏠_Home.py
+++ 1_🏠_Home.py
@@ -31,18 +31,22 @@ EXPERIENCE="""
 
 """
 SKILL="""
-<div style="width: 1600px; padding: 0px;"</div>
+<div style="width: 1400px;"</div>
 <h1 style="text-align: left; text-indent: 60px; color: white; font-size: 40px; text-shadow: 5px 5px 10px #000000; font-family: "Fantasy">
 
-🏅Team player/leader capable of manage multidiscipline modellers and drafters. Recently working as project BIM manager on multidiscipline projects and managing modellers from around Australia and overseas
+🏅Team player/leader capable of manage multidiscipline modellers and drafters. 
+Recently working as project BIM manager on multidiscipline projects and managing modellers from around Australia and overseas
 
-🏅Define workflow, create standard, implementation standards, manage the resources, estimate time. Capable to provide training locally or remotely for implementation of standards. Great communication skills
+🏅Define workflow, create standard, implementation standards, manage the resources, estimate time. 
+Capable to provide training locally or remotely for implementation of standards. Great communication skills
 
 🏅Passionate about BIM implementation not only in modelling but also in assess management, cost estimate and its impact on design procedure
 
-🏅AutoCAD Plant 3D, Revit and Civil 3D customisation, utilizing programming to reduce manual works and increase productivity, use Python to create parametric catalogue items
+🏅AutoCAD Plant 3D, Revit and Civil 3D customisation, utilizing programming to reduce manual works and increase productivity, 
+use Python to create parametric catalogue items
 
-🏅Knowing how to use and understanding the pros and cons of many software including Plant 3D, Civil 3D, Revit, Inventor, Solidworks, Infraworks and Navisworks. Able to create templates, parametric catalogue items, families for design software
+🏅Knowing how to use and understanding the pros and cons of many software including Plant 3D, Civil 3D, Revit, 
+Inventor, Solidworks, Infraworks and Navisworks. Able to create templates, parametric catalogue items, families for design software
 
 </h1>
 
